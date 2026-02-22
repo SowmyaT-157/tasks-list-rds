2904cd45f2d2d39b14ab0fd52a97b3acf7ef3146
sowmya <sowmya.t@everest.engineering>
2026-02-22 10:23:21 +0530

    Update the controllers for add the post method

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { allTasks } from "../services/tasksServices";
+import { allTasks, createNewTask } from "../services/tasksServices";
 
 export const handleTaskList = async(req:Request,res:Response) =>{
     try {
@@ -12,3 +12,18 @@ export const handleTaskList = async(req:Request,res:Response) =>{
     }
 
 }
+
+export const createTask = async(req:Request,res:Response) =>{
+     try {
+        const data = req.body
+        const newTask = await createNewTask(data)
+        if (newTask) {
+            return res.status(201).json({ message: "successfully created", newTask })
+        } else {
+            return res.status(404).json({ message: "bad request" })
+        }
+    } catch (error) {
+        return res.status(500).json({ message: "network issue", error })
+    }
+}
+
