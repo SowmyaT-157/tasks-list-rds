a93ef237b7f08c43c41f7ddaca05fadaa7c699be
sowmya <sowmya.t@everest.engineering>
2026-02-22 17:19:44 +0530

    Update the config to connect the local

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -3,12 +3,12 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const sequelize = new Sequelize(
- process.env.L_DATABASE as string,
- process.env.L_USERNAME as string,
- process.env.L_PASSWORD as string,
+ process.env.DB_DATABASE as string,
+ process.env.DB_USERNAME as string,
+ process.env.DB_PASSWORD as string,
     {
-    host: process.env.L_HOST as string,
-    dialect: process.env.L_DIALECT as Dialect,
+    host: process.env.DB_HOST as string,
+    dialect: process.env.DB_DIALECT as Dialect,
 });
 export default sequelize;
 
