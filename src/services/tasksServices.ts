d6bd8a5152da0f64ef8b6f2828514cf2f381f381
sowmya <sowmya.t@everest.engineering>
2026-02-22 10:25:20 +0530

    Update the services for add post method

--- src/services/tasksServices.ts
+++ src/services/tasksServices.ts
@@ -1,6 +1,20 @@
 import { Task } from "../models/taskModal";
 
+type taskDetails ={
+   task_name:string,   
+   description:string,
+   priority:string,
+   status:string  
+}
+
 export const allTasks = async () => {
   const task = await Task.findAll()
   return task;
 }
+
+export const createNewTask = async (taskData: taskDetails) => {
+  const newTaskList = Task.create(taskData)
+  return taskData
+}
+
+
