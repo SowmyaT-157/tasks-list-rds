d556b90f5e2cbaf3aa584e2befa2b57e66f1704a
sowmya <sowmya.t@everest.engineering>
2026-02-25 00:57:55 +0530

    Update the controllers to update the post method

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -16,12 +16,12 @@ export const handleTaskList = async(req:Request,res:Response) =>{
 export const createTask = async(req:Request,res:Response) =>{
      try {
         const data = req.body
+        console.log("giving re",data)
         const newTask = await createNewTask(data)
+        console.log("tasks coming",newTask)
         if (newTask) {
             return res.status(201).json({ message: "successfully created", newTask })
-        } else {
-            return res.status(404).json({ message: "bad request" })
-        }
+        } 
     } catch (error) {
         return res.status(500).json({ message: "network issue", error })
     }
